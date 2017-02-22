2dedea137a99ccfe42b837a7bf672953ae3dba1e
cikai <cikai0923@gmail.com>
2017-02-22 10:23:43 +0800

    koa2 project

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-var fs = require("fs")
-var markdown = require("markdown").markdown;
+const Koa = require('koa');
+const app = new Koa();
+const router = require('koa-router')();
+const views = require('koa-views');
+const co = require('co');
+const convert = require('koa-convert');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser')();
+const logger = require('koa-logger');
 
-var markdown_dir = "app/markdown/";
-var html_dir = "app/html/";
-var file_name = "http-server.md";
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var md = fs.readFileSync(markdown_dir + file_name);
-var html = markdown.toHTML(md.toString());
-fs.writeFile(html_dir + file_name, html, function(err) {
- if (err) {
-   return console.error(err);
- }
- console.log("写入成功！");
+// middlewares
+app.use(convert(bodyparser));
+app.use(convert(json()));
+app.use(convert(logger()));
+app.use(require('koa-static')(__dirname + '/public'));
+
+app.use(views(__dirname + '/views', {
+  extension: 'jade'
+}));
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
+
+router.use('/', index.routes(), index.allowedMethods());
+router.use('/users', users.routes(), users.allowedMethods());
+
+app.use(router.routes(), router.allowedMethods());
+// response
+
+app.on('error', function(err, ctx){
+  console.log(err)
+  logger.error('server error', err, ctx);
+});
+
+
+module.exports = app;
